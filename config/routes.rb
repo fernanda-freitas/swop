0102aeb13d148496cc43436ffa53c5e5a56da1f5
fernanda freitas <idea.fernanda@gmail.com>
2022-08-06 22:47:02 +0100

    products controller model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :products, except: :destroy
   # Defines the root path route ("/")
   # root "articles#index"
 end
