f3e08d29977e6b8baca3481323c2b229011506d9
fernanda freitas <idea.fernanda@gmail.com>
2022-08-07 18:21:05 +0100

    new product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,14 +8,28 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :category, :description, :price)
   end
 end
