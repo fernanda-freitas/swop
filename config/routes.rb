4f454075d49228a559cde4024b03790b3323825a
fernanda freitas <idea.fernanda@gmail.com>
2022-08-08 10:58:54 +0100

    destroy product method and buttons frontend

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products, except: :destroy
+  resources :products
   # Defines the root path route ("/")
   # root "articles#index"
 end
