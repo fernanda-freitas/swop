5a3d375969c16744b07ff43f8a109a27e6e62374
fernanda freitas <idea.fernanda@gmail.com>
2022-08-09 11:41:08 +0100

    set cloudinary config to initializers

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dlg0k4vhd'
+  config.api_key = '223972414148277'
+  config.api_secret = 'SrPIfdmyOUS5ytv8MGBPcTlKqrE'
+  config.secure = true
+end
